0f44a2de4a2a031ed38426ac1cf88e203c0be80e
William Crump <williamcrump@Williams-MacBook-Pro-3.local>
2020-03-19 13:00:18 -0700

    tweaks to make sure it works

--- R/manhattan.plot.function.R
+++ R/manhattan.plot.function.R
@@ -1,6 +1,6 @@
 #' Generate a Manhattan Plot of a GWAS result
 #'
-#' @param marker_map A data.frame containing marker 'rs', 'chr', and 'pos' (should be in the same order as the GWAS results)
+#' @param marker_map A data.frame containing markers with 3 columns: taxa, chromosome, and position (rows should be in the same order as the GWAS results)
 #' @param pvals The untransformed p-values from a GWAS test
 #' @param QTN_index The indices of any known QTNs
 #' @param trait The name of the trait
@@ -8,12 +8,12 @@
 
 manhattan_plot <- function(marker_map, pvals, QTN_index = c(), trait = "unknown")
 {
-	marker_map$pvals <- -log10(pvals) # Add pvalues to the data.frame and log10 transform
+	marker_map$pvals <- -log10(t(pvals)) # Add pvalues to the data.frame and log10 transform
 
-	marker_map$comb_pos <- marker_map$chrom * 1e9 + marker_map$pos
-	manhattan_plot <- ggplot(marker_map, aes(x = 1:nrow(marker_map), y = pvals, color = factor(chrom))) +
+	marker_map$comb_pos <- marker_map$Chromosome * 1e9 + marker_map$Position
+	manhattan_plot <- ggplot(marker_map, aes(x = 1:nrow(marker_map), y = pvals, color = factor(Chromosome))) +
 		geom_point() +
-		geom_vline(xintercept = QTN_index, color = "red") +
+		#geom_vline(xintercept = QTN_index, color = "red") +
 		labs(title = paste("GWAS manhattan plot for trait:", trait),
 			y = "-log10(p-value)",
 			x = "Marker Position",

3f7eb95fcd6cf11ff01d87c4f51359dbd2b9df68
William Crump <williamcrump@Williams-MacBook-Pro-3.local>
2020-03-18 22:58:23 -0700

    added threshold parameter to allow users to input the max correlation they want

--- R/filter.pca.function.R
+++ R/filter.pca.function.R
@@ -2,10 +2,11 @@
 #'
 #' @param PCA The output list from the prcomp() function.
 #' @param covs A matrix/data.frame of covariates (rows = individuals, columns = covariates)
+#' @param threshold A numerical value (0-1) indicating the maximum allowed correlation between any one covariate and any one PC
 #' @return A matrix of filtered PCs (not correlated with any covariates supplied by user)
 
 
-filter.pca <- function(PCA, covs){
+filter.pca <- function(PCA, covs, threshold){
 	library(tidyverse)
 
 	# ensure the first column is the first covariate
@@ -14,7 +15,7 @@ filter.pca <- function(PCA, covs){
 	}
 
 	correlation.matrix <- cor(PCA$x, covs)
-	indices <- apply(correlation.matrix, 2, function(x){x>=0.5})
+	indices <- apply(correlation.matrix, 2, function(x){x>=threshold})
 	index.vect <- apply(indices, 2, function(x){which(x)})
 
 	PCA$x <- PCA$x[,-index.vect]

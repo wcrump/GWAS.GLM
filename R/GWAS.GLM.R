fbaf2602f0cf62968b753900df026e7cea1461b2
wcrump <54826007+wcrump@users.noreply.github.com>
2020-03-23 14:37:22 -0700

    Update GWAS.GLM.R

--- R/GWAS.GLM.R
+++ R/GWAS.GLM.R
@@ -1,6 +1,7 @@
 #' GWAS.GLM: A package for performing genome-wide associations by general linear modeling.
 #'
 #' The GWAS.by.GLM package consists of one main function that uses several side functions.
+#'
 #' The main function is:
 #' GWAS.by.GLM
 #'

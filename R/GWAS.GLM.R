c10218ccbd6edebd806ec2874a06430e64cf647f
William Crump <williamcrump@Williams-MacBook-Pro-3.local>
2020-03-23 14:23:53 -0700

    package help info

---
+++ R/GWAS.GLM.R
@@ -0,0 +1,9 @@
+#' GWAS.GLM: A package for performing genome-wide associations by general linear modeling.
+#'
+#' The GWAS.by.GLM package consists of one main function that uses several side functions.
+#' The main function is:
+#' GWAS.by.GLM
+#'
+#' @docType package
+#' @name GWAS.GLM
+NULL
